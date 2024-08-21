e7570f6680e4deaacd3955ed261a915385713570
unknown <j.bishop@reading.ac.uk>
2024-08-21 08:27:36 +0100

    saving processed dataset

---
+++ R/jake analysis.R
@@ -0,0 +1,6 @@
+# 210824
+# I am running this after running the Yield_dat_2024-08-20 script
+# reading in the processed file
+
+data <- read.csv('data/data_processed.csv', sep = ',', dec = '.')
+colnames(data)
