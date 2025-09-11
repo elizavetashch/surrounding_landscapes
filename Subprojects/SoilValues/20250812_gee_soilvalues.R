e602914dd1a0e7b7c099c05da8c25f6b947d021c
elizaveta <shcherbinina.elisaveta@gmail.com>
2025-09-11 10:03:45 +0200

    soilvalues update

--- Subprojects/SoilValues/20250812_gee_soilvalues.R
+++ Subprojects/SoilValues/20250812_gee_soilvalues.R
@@ -1,7 +1,11 @@
 
+# Original 20250813
+# Modified 20250911
 
 library(dplyr)
-# Data read in ------------------------------------------------------------
+library(tidyr)
+
+# Data read in 
 
 df_1000 <- read.csv("C:\\Users\\lisa7\\Documents\\UFZ_CLE\\surrounding_landscapes_full_project\\20250812_surrounding_landscapes\\data\\20250812_soilvalues_1000m.csv")
 df_2500 <- read.csv("C:\\Users\\lisa7\\Documents\\UFZ_CLE\\surrounding_landscapes_full_project\\20250812_surrounding_landscapes\\data\\20250812_soilvalues_2500m.csv")
@@ -20,9 +24,28 @@ df_full$buffer_area <- pi * (df_full$buffer_radius ^ 2)
 df_full$.geo <- NULL
 df_full$system.index <- NULL
 
-df_20250813 <- 
-  left_join(df_current, df_full, join_by("ma_id"=="ma_id", "study_id"=="study_id", "measurement_id"=="measurement_id"))
+# Organize the column order
+d <- df_full %>% 
+  select(-buffer_area)
+d <- d %>% select(ma_id, measurement_id,study_id,buffer_radius,
+                  bdod_bdod_0.5cm_mean_mean:clay_clay_60.100cm_mean_mean,
+                  nitrogen_nitrogen_0.5cm_mean_mean:soc_soc_60.100cm_mean_mean)
+# Check
+glimpse(d)
 
-summary(df_20250813)
+# Pivot wider
+d2 <- d %>% 
+  pivot_wider(
+  names_from = buffer_radius,
+  values_from = c(bdod_bdod_0.5cm_mean_mean:soc_soc_60.100cm_mean_mean),
+  names_glue = "{.value}.{buffer_radius}")
 
-write.csv(df_20250813, "C:\\Users\\lisa7\\Documents\\UFZ_CLE\\surrounding_landscapes_full_project\\20250812_surrounding_landscapes\\data\\20250813_data.csv")
+# Join
+df_20250911 <- 
+  left_join(df_current, d2, join_by("ma_id"=="ma_id", "study_id"=="study_id", "measurement_id"=="measurement_id"))
+
+# Check
+glimpse(df_20250911)
+
+# Write
+write.csv(df_20250911, "C:\\Users\\lisa7\\Documents\\UFZ_CLE\\surrounding_landscapes_full_project\\20250812_surrounding_landscapes\\data\\20250911_soildata.csv")
