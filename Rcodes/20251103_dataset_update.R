4f1bd2e60a6d5a359607d59b7ae06d7c1b23d973
elizaveta <shcherbinina.elisaveta@gmail.com>
2025-11-03 14:47:50 +0100

    update data

--- Rcodes/20251103_dataset_update.R
+++ Rcodes/20251103_dataset_update.R
@@ -61,45 +61,97 @@ soil2500$radius_m <- 2500
 soil5000 <- read.csv("C:\\Users\\lisa7\\Documents\\UFZ_CLE\\surrounding_landscapes_full_project\\20250812_surrounding_landscapes\\data\\GEE_SoilGrids_20251103\\20251102_soilvalues_5000m.csv", header = TRUE, sep = ",")
 soil5000$radius_m <- 5000
 
-# merging
+
+# Organize the datasets ---------------------------------------------------
+
 
 # old
 glimpse(old)
 p1 <- old[2:79]
 p2 <- old[245:267]
+old <- bind_cols(p1,p2)
+# AM
+AM_full <- bind_rows(AM1000,AM2500,AM5000) # bind rows 
+AM_full <- AM_full%>% select(ma_id, measurement_id,study_id,radius_m, # order the columns
+                               pixel_first, stdDevfungi, varfungi,fungi) %>% 
+                        rename(
+                              fungi_firstpixel = pixel_first,
+                              fungi_stdDev = stdDevfungi,
+                              fungi_variance = varfungi,
+                              fungi_type = fungi
+                              )
+AM_full_wide <- AM_full %>% # radius to wide format
+  pivot_wider(
+    names_from = c(radius_m, fungi_type),
+    values_from = fungi_firstpixel:fungi_variance,
+    names_glue = "{fungi_type}.{.value}.{radius_m}"
+  )
+# EcM
+EcM_full <- bind_rows(EcM1000,EcM2500,EcM5000) # bind rows 
+EcM_full <- EcM_full%>% select(ma_id, measurement_id,study_id,radius_m, # order the columns
+                             pixel_first, stdDevfungi, varfungi,fungi) %>% 
+                      rename(
+                        fungi_firstpixel = pixel_first,
+                        fungi_stdDev = stdDevfungi,
+                        fungi_variance = varfungi,
+                        fungi_type = fungi
+                      )
+EcM_full_wide <- EcM_full %>% # radius to wide format
+  pivot_wider(
+    names_from = c(radius_m, fungi_type),
+    values_from = fungi_firstpixel:fungi_variance,
+    names_glue = "{fungi_type}.{.value}.{radius_m}"
+  )
+
+# merge AM and EcM
+AM_EcM <- left_join(
+  AM_full_wide,
+  EcM_full_wide,
+  by = c("ma_id", "study_id", "measurement_id")
+)
 
 # soil
-df_full <- bind_rows(df_1000,df_2500,df_5000)
-df_full$buffer_radius <- as.numeric(df_full$buffer_radius)
-df_full$buffer_area <- pi * (df_full$buffer_radius ^ 2)
-df_full$.geo <- NULL
-df_full$system.index <- NULL
-
-# Organize the column order
-d <- df_full %>% 
-  select(-buffer_area)
-d <- d %>% select(ma_id, measurement_id,study_id,buffer_radius,
-                  bdod_bdod_0.5cm_mean_mean:clay_clay_60.100cm_mean_mean,
-                  nitrogen_nitrogen_0.5cm_mean_mean:soc_soc_60.100cm_mean_mean)
-# Check
-glimpse(d)
-
-# Pivot wider
-d2 <- d %>% 
+soil_full <- bind_rows(soil1000,soil2500,soil5000) # bind rows 
+soil_full <- soil_full%>% select(ma_id, measurement_id,study_id,radius_m, # order the columns
+                  bdod_bdod_0.5cm_mean_firstpixel:clay_clay_60.100cm_mean_variance,
+                  nitrogen_nitrogen_0.5cm_mean_firstpixel:soc_soc_60.100cm_mean_variance)
+soil_full_wide <- soil_full %>% # radius to wide format
   pivot_wider(
-    names_from = buffer_radius,
-    values_from = c(bdod_bdod_0.5cm_mean_mean:soc_soc_60.100cm_mean_mean),
-    names_glue = "{.value}.{buffer_radius}")
+    names_from = radius_m,
+    values_from = c(bdod_bdod_0.5cm_mean_firstpixel:soc_soc_60.100cm_mean_variance),
+    names_glue = "{.value}.{radius_m}")
 
-# Join
-df_20250911 <- 
-  left_join(df_current, d2, join_by("ma_id"=="ma_id", "study_id"=="study_id", "measurement_id"=="measurement_id"))
+# Join the Soil Texture  ---------------------------------------------
+
+soiltexture <- read.csv("data/20251006_df_soiltype_unscaled.csv")
+glimpse(soiltexture)
+soiltexture <- soiltexture %>% 
+  select(ma_id, measurement_id,study_id, SAND, SILT, CLAY, soiltype) %>% 
+  rename(soiltexture = soiltype,
+         sand.percent.1000 = SAND, 
+         silt.percent.1000 = SILT,
+         clay.percent.1000 = CLAY)
+
+
+# Join the data -----------------------------------------------------------
+
+df_20251103 <- 
+  left_join(old, AM_EcM, join_by("ma_id", "study_id", "measurement_id"))
+
+df_20251103 <- 
+  left_join(df_20251103, soil_full_wide, join_by("ma_id", "study_id", "measurement_id"))
+
+df_20251103 <- 
+  left_join(df_20251103, soiltexture, join_by("ma_id", "study_id", "measurement_id"))
 
 # Check
-glimpse(df_20250911)
+glimpse(df_20251103)
 
 # Write
-write.csv(df_20250911, "C:\\Users\\lisa7\\Documents\\UFZ_CLE\\surrounding_landscapes_full_project\\20250812_surrounding_landscapes\\data\\20250911_soildata.csv",
+write.csv(df_20251103, "C:\\Users\\lisa7\\Documents\\UFZ_CLE\\surrounding_landscapes_full_project\\20250812_surrounding_landscapes\\data\\20251103_data.csv",
+          row.names = FALSE)
+# Write to Git 
+write.csv(df_20251103, ".\\data\\20251103_data.csv",
           row.names = FALSE)
 
 
